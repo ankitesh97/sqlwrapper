45628d1deac0d868c016eeed42dab36e08ea7213
Ankitesh Gupta <ankiteshguptas@gmail.com>
2017-02-03 21:49:52 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'sqlwrapper',
   packages = ['sqlwrapper'],
-  version = '1.0',
-  description = 'A sql wrapper for those who have a hard time executing queries, it provides an interface to simulate common queries on a database',
+  version = '0.1',
+  description = ' This module is a wrapper aroung SQL query language, i.e an interface between the database and python.',
   author = 'Ankitesh Gupta, Saif Vazir',
   author_email = 'ankiteshguptas@gmail.com,ssv.saif@gmail.com',
   url = 'https://github.com/ankitesh97/sqlwrapper',
-  download_url = 'https://github.com/ankitesh97/sqlwrapper/tarball/v1.0',
-  keywords = ['sql', 'sqlite', 'database', 'queries', 'sqlwrapper','sql'],
+  download_url = 'https://github.com/ankitesh97/sqlwrapper/tarball/v0.1',
+  keywords = ['sql', 'sqlite', 'database', 'queries', 'sqlwrapper','sql', 'mysql', 'postgresql'],
   classifiers = [],
 )
